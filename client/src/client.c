a6082e91e7e85064aa25d1d6dfce58e93dbeec64
martinAguirreRuiz <131924253+martinAguirreRuiz@users.noreply.github.com>
2024-04-05 10:21:56 -0300

    Actualizo iniciar_config de client.c

--- client/src/client.c
+++ client/src/client.c
@@ -86,7 +86,7 @@ t_log* iniciar_logger(void)
 
 t_config* iniciar_config(void)
 {
-	t_config* nuevo_config = config_create("tp0.config");
+	t_config* nuevo_config = config_create("../tp0.config");
 
 	if (nuevo_config == NULL)
 	{
